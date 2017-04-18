3a0d11e342ae14ee9393c8da87e2d8a77ae91171
RydenButler <ryden.winfield@gmail.com>
2017-04-18 11:06:02 -0500

    Comment Function
    - Add comments
    - Minor re-formatting
    - Test on sin function

--- PS6.R
+++ PS6.R
@@ -1,30 +1,37 @@
-sg.int<-function(g,...,lower,upper){
+sg.int <- function(g,...,lower,upper){
   require("SparseGrid")
-  
-  lower<-floor(lower)
-  
-  upper<-ceiling(upper)
-  
-  if (any(lower>upper)) stop("lower must be smaller than upper")
-  
-  gridss<-as.matrix(expand.grid(seq(lower[1],upper[1]-1,by=1),seq(lower[2],upper[2]-1,by=1)))
-  
-  sp.grid <- createIntegrationGrid( 'KPU', dimension=2, k=5 )
-  
-  nodes<-gridss[1,]+sp.grid$nodes
-  
-  weights<-sp.grid$weights
-  
-  for (i in 2:nrow(gridss))
-    
-  {
-    nodes<-rbind(nodes,gridss[i,]+sp.grid$nodes)  
-    
-    weights<-c(weights,sp.grid$weights)
-    
+  # Set lower bound over which to integrate (must be length of dimensions)
+  lower <- floor(lower)
+  # Set upper bound over which to integrate (must be length of dimensions)
+  upper <- ceiling(upper)
+  # Throw error if the lower bound is greater than the upper bound
+  if (any(lower > upper)) stop("lower must be smaller than upper")
+  # Create matrix of all combinations of supplied vectors
+  gridss <- as.matrix(expand.grid(seq(lower[1], upper[1]-1, by=1),
+                                  seq(lower[2], upper[2]-1, by=1)))
+  # Creates nodes and weights for integration; 
+  # KPU method is nested rule for unweighted integral over [0,1]
+  # k is the accuracy level of integration
+  sp.grid <- createIntegrationGrid( 'KPU', dimension = 2, k = 5)
+  # Create node values for lower bounds of integration
+  nodes <- gridss[1, ] + sp.grid$nodes
+  # Store node weight values for lower bounds of interation
+  weights <- sp.grid$weights
+  # Create node values for all other points of integration
+  for (i in 2:nrow(gridss)) {
+    # Create node values for combination of points i
+    nodes <- rbind(nodes, gridss[i, ] + sp.grid$nodes)  
+    # Store node weight values for combination i
+    weights <- c(weights, sp.grid$weights)
   }
-  
+  # Evaluate function over each set of nodes
   gx.sp <- apply(nodes, 1, g,...)
+  # Weight values and sum rows
   val.sp <- gx.sp %*%weights
+  # Return values
   val.sp
 }
+
+sg.int(sin, lower= c(-1,-1), upper = c(1,1))
+
+
